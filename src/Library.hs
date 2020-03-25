14dc3474f3b759b970a2741ebe296ecfd512566c
Juan José Contardo <juan.contardo@gmail.com>
2020-03-25 17:52:51 -0300

    fix name

--- src/Library.hs
+++ src/Library.hs
@@ -1,7 +1,7 @@
 module Library where
 import PdePreludat
 
-bonoPara :: Number -> Number
-bonoPara sueldo | sueldo < 10000 = 10000
+bono :: Number -> Number
+bono sueldo | sueldo < 10000 = 10000
  | sueldo < 30000 = 5000
  | otherwise = 0
--- test/Spec.hs
+++ test/Spec.hs
@@ -6,8 +6,8 @@ main :: IO ()
 main = hspec $ do
   describe "Test de pago" $ do
     it "Si una persona cobra menos del mínimo, el bono es del valor correcto" $ do
-      10000 `shouldBe` bonoPara 9999
+      10000 `shouldBe` bono 9999
     it "Si una persona cobra entre el mínimo y el máximo, el bono es del valor correcto" $ do
-      5000 `shouldBe` bonoPara 29999
+      5000 `shouldBe` bono 29999
     it "Si una persona cobra más del máximo, el bono es del valor correcto" $ do
-      0 `shouldBe` bonoPara 30001
+      0 `shouldBe` bono 30001
